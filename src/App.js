d01eb5f24b9c4f07eb1fb348e9be212ed3281714
pungjung10 <pungjungg@gmail.com>
2021-09-17 02:33:16 +0700

    Counter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';    //allow to use jsx code(tell compiler)
+import Counter from './Counter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Welcome to my counter :)</h2>
+        <Counter/>
       </header>
     </div>
   );
